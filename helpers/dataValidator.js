15ff4abeba4f36456dc980fceb0be25ab2460e6d
andreas7500 <andreas2875@gmail.com>
2023-07-17 20:32:40 +0300

    added changes

--- helpers/dataValidator.js
+++ helpers/dataValidator.js
@@ -1,94 +1,107 @@
-const Joi = require("joi");
+// const Joi = require("joi");
 
 // Joi validation schemas
-const schemaCreateContact = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-  phone: Joi.string().min(7).max(27).required(),
-});
+// const schemaCreateContact = Joi.object({
+//   name: Joi.string().min(3).max(30).required().messages({
+//     "any.required": `missing required name field`,
+//   }),
+//   email: Joi.string()
+//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+//     .required()
+//     .messages({
+//       "any.required": `missing required email field`,
+//     }),
+//   phone: Joi.string()
+//     .min(7)
+//     .max(27)
+//     .required()
+//     .messages({ "any.required": `missing required phone field` }),
+// });
 
-const schemaUpdateContact = Joi.object({
-  name: Joi.string().min(3).max(30).optional(),
+// const schemaUpdateContact = Joi.object({
+//   name: Joi.string().min(3).max(30).required().messages({
+//     "any.required": `missing required name field`,
+//   }),
+//   email: Joi.string()
+//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+//     .required()
+//     .messages({
+//       "any.required": `missing required email field`,
+//     }),
+//   phone: Joi.string()
+//     .min(7)
+//     .max(27)
+//     .required()
+//     .messages({ "any.required": `missing required phone field` }),
+// });
+
+// POST method validation middleware
+// function validateCreateContact(req, res, next) {
+//   const { error } = schemaCreateContact.validate(req.body);
+//   if (error) {
+//     const errorMessage = `Missing required ${error.details[0].context.label} field`;
+//     return res.status(400).json({ message: errorMessage });
+//   }
+//   next();
+// }
+
+// PUT method validation middleware
+
+// function validateUpdateContact(req, res, next) {
+//   if (!Object.keys(req.body).length) {
+//     return res.status(400).json({ message: "Missing fields" });
+//   }
+
+//   const { error } = schemaUpdateContact.validate(req.body);
+//   if (error) {
+//     const errorMessage = `Missing required ${error.details[0].context.label} field`;
+//     return res.status(400).json({ message: errorMessage });
+//   }
+//   next();
+// }
+const Joi = require("joi");
+
+const schemaValidate = Joi.object({
+  name: Joi.string().min(3).max(30).required().messages({
+    "any.required": `missing required name field`,
+  }),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .optional(),
-  phone: Joi.string().min(7).max(27).optional(),
+    .required()
+    .messages({
+      "any.required": `missing required email field`,
+    }),
+  phone: Joi.string()
+    .min(7)
+    .max(27)
+    .required()
+    .messages({ "any.required": `missing required phone field` }),
 });
 
 // POST method validation middleware
 function validateCreateContact(req, res, next) {
-  const { error } = schemaCreateContact.validate(req.body);
+  const { error } = schemaValidate.validate(req.body);
   if (error) {
-    const errorMessage = `Missing required ${error.details[0].context.label} field`;
+    const errorMessage = error.message;
     return res.status(400).json({ message: errorMessage });
   }
+
   next();
 }
 
 // PUT method validation middleware
 function validateUpdateContact(req, res, next) {
-  if (!req.body) {
+  if (!Object.keys(req.body).length) {
     return res.status(400).json({ message: "Missing fields" });
   }
 
-  const { error } = schemaUpdateContact.validate(req.body);
+  const { error } = schemaValidate.validate(req.body);
   if (error) {
-    const errorMessage = `Missing required ${error.details[0].context.label} field`;
+    const errorMessage = error.message;
     return res.status(400).json({ message: errorMessage });
   }
+
   next();
 }
 
 module.exports = { validateCreateContact, validateUpdateContact };
-
-// рррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррррр
-
-// const Joi = require("joi");
-
-// const schemaCreateContact = Joi.object({
-//   name: Joi.string().min(3).max(30).required(),
-//   email: Joi.string()
-//     .email({
-//       minDomainSegments: 2,
-//       tlds: { allow: ["com", "net"] },
-//     })
-//     .required(),
-//   phone: Joi.string().min(7).max(27).required(),
-// });
-
-// const schemaUpdateContact = Joi.object({
-//   name: Joi.string().min(3).max(30).optional(),
-//   email: Joi.string()
-//     .email({
-//       minDomainSegments: 2,
-//       tlds: { allow: ["com", "net"] },
-//     })
-//     .optional(),
-//   phone: Joi.string().min(7).max(27).optional(),
-// });
-
-// function validateCreateContact(req, res, next) {
-//   const { error } = schemaCreateContact.validate(req.body);
-//   if (error) {
-//     return res.status(400).json({
-//       message: `Missing required ${error.details[0].context.label} field`,
-//     });
-//   }
-//   next();
-// }
-
-// function validateUpdateContact(req, res, next) {
-//   const { error } = schemaUpdateContact.validate(req.body);
-//   console.log(req.body);
-//   if (error) {
-//     return res.status(400).json({ message: "Missing fields" });
-//   }
-//   next();
-// }
-
-// module.exports = {
-//   validateCreateContact,
-//   validateUpdateContact,
-// };
