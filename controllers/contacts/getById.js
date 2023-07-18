7f0022819d53a93a5c2e1ad345cd434aaac88ccf
andreas7500 <andreas2875@gmail.com>
2023-07-18 16:44:46 +0300

    added new change

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,12 +1,12 @@
 const contacts = require("../../models/contacts");
-const { HttpError } = require("../../helpers");
+// const { HttpError } = require("../../helpers");
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
   const result = await contacts.getContactById(contactId);
   if (!result) {
-    throw HttpError(404, "Not found");
-    // return res.status(404).json({ message: "not found" });
+    // throw HttpError(404, "Not found");
+    return res.status(404).json({ message: "not found" });
   }
 
   res.json(result);
