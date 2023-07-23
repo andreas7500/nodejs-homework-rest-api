a7b361a044ec8741f89497aa4ec761ca766c821f
andreas7500 <andreas2875@gmail.com>
2023-07-23 20:26:37 +0300

    fix message

--- controllers/contacts/updateFavorite.js
+++ controllers/contacts/updateFavorite.js
@@ -1,15 +1,17 @@
 const { HttpError } = require("../../helpers");
 const { Contact } = require("../../models/contact");
 
-const updateFavorite = async (req, res) => {
+const updateFavorite = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
   if (!result) {
     throw HttpError(404, `Movie with id=${contactId} not found`);
+
     // return res.status(404).json({ message: "not found" });
   }
+
   res.json(result);
 };
 
--- middlewares/isEmptyBody.js
+++ middlewares/isEmptyBody.js
@@ -1,9 +1,17 @@
 const { HttpError } = require("../helpers/index");
 
 const isEmptyBody = (req, res, next) => {
+  if (req.method === "PATCH") {
+    const { length } = Object.keys(req.body);
+    if (!length) {
+      return res.status(400).json({ message: "missing field favorite" });
+      // return next(HttpError(400, "missing field favorite"));
+    }
+  }
+
   const { length } = Object.keys(req.body);
   if (!length) {
-    next(HttpError(400, "missing fields"));
+    return next(HttpError(400, "missing fields"));
   }
   next();
 };
